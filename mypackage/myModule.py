12b4f92012fe3b420c050afffa6fb4a1c71367e5
wrheeder@cellc.co.za <WRheeder@ZA.CellC.net>
2020-02-27 11:39:22 +0200

    First Commit

---
+++ mypackage/myModule.py
@@ -0,0 +1,15 @@
+def top_n(items, n):
+    """Return the top n items in an array, in descending order.
+
+    Args:
+        items (array): list or array-like object containing numerical values.
+        n (int): number of top items to return.
+
+    Returns:
+        array: top n items, in descending order.
+
+    Examples:
+        >>> top_n([8, 3, 2, 7, 4], 3)
+        [8, 7, 3]
+    """
+    return sorted(items,reverse=True)[:n]
